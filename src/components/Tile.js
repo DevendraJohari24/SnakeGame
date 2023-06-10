fd6f0ff61701b7ebf7d26ae16bc69663100633ca
Devendra Johari <54894480+DevendraJohari24@users.noreply.github.com>
2023-06-11 02:41:13 +0530

    commit

---
+++ src/components/Tile.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class Tile extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            style: {
+                aspectRatio: "1",
+                float: "left"
+            }
+        }
+    }
+
+    static getDerivedStateFromProps(props, state){
+        return {
+            style: {backgroundColor: props.color, aspectRatio: "1", float: "left"}
+        }
+    }
+
+    render() {
+    
+    return (
+      <div className='game-tile' style={this.state.style}>
+      </div>
+    )
+  }
+}
